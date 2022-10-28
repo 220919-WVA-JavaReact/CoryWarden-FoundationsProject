aff1fbb0f19f9f5761542cef54f90bca607d46ec
Cory W <12cwarden@gmail.com>
2022-10-28 15:45:07 -0400

    established start point for spring boot

---
+++ Java/FoundationsProject/src/main/java/com/revature/ERSApp.java
@@ -0,0 +1,11 @@
+package com.revature;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ERSApp {
+    public static void main(String[] args) {
+        SpringApplication.run(ERSApp.class, args);
+    }
+}
